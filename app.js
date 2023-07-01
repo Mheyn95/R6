3e65c0571ac2f443f84c0e2113f7f8452bcc8a66
mheyn <mheyn@dentalintel.com>
2023-07-01 15:55:11 -0600

    Boilerplate

---
+++ app.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#app',
+    data: {
+      message: 'Welcome to Your Vue App!',
+    },
+    template: `
+      <div>
+        <h1>{{ message }}</h1>
+      </div>
+    `,
+  });
